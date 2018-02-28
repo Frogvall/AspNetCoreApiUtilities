8e24d783e67d5ff51902a5b05b26f4b93982a777
Frogvall <frjo02@gmail.com>
2018-02-28 10:00:01 +0100

    Removed annoying line break

--- src/AspNetCoreApiUtilities/Extensions/ApiExceptionHandlerExtensions.cs
+++ src/AspNetCoreApiUtilities/Extensions/ApiExceptionHandlerExtensions.cs
@@ -7,13 +7,12 @@ namespace Microsoft.AspNetCore.Builder
 {
     public static class ApiExceptionHandlerExtensions
     {
-        public static IApplicationBuilder UseApiExceptionHandler(
-            this IApplicationBuilder builder)
-        {            
+        public static IApplicationBuilder UseApiExceptionHandler(this IApplicationBuilder builder)
+        {
             return builder.UseExceptionHandler(new ExceptionHandlerOptions
-            {                
+            {
                 ExceptionHandler = new ApiExceptionHandler(
-                    builder.ApplicationServices.GetRequiredService<IHostingEnvironment>(), 
+                    builder.ApplicationServices.GetRequiredService<IHostingEnvironment>(),
                     builder.ApplicationServices.GetRequiredService<ILogger<ApiExceptionHandler>>())
                         .ExceptionHandler
             });
